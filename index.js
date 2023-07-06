349fd5239f44430d6171072b379b1813f6f30bb1
Eli <emmuzio0510@icloud.com>
2023-07-06 14:16:32 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function addCat(catName) {
+  cats.push(catName);
+}
+
+function removeFirstCat() {
+  cats.shift();
+}
+
+function removeLastCat() {
+  cats.pop();
+}
+
+function getCatsCount() {
+  return cats.length;
+}
+
+function hasCat(catName) {
+  return cats.includes(catName);
+}
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+
+  function appendCat(name) {
+    return [...cats, name];
+  }
+
+  function prependCat(name) {
+    return [name, ...cats];
+  }
+
+  function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+  }
+  
+  function removeFirstCat() {
+    return cats.slice(1);
+  }
